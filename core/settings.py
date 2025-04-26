d5db013c7d5ece7db1c462718f2edc31a49e54ad
faidercamilo26 <olayafaider@gmail.com>
2025-04-25 19:34:33 -0500

    Banner updated to show static

--- core/settings.py
+++ core/settings.py
@@ -179,7 +179,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build'),
+    os.path.join(BASE_DIR, 'build/images'),
     
 ]
 
