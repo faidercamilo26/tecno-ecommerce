c85185a400e3ab3520804017e8eefaf23d0d30b9
faidercamilo26 <olayafaider@gmail.com>
2025-04-27 13:38:23 -0500

    The was added settings for cloudstorage

--- core/settings.py
+++ core/settings.py
@@ -245,7 +245,7 @@ DJOSER = {
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')  # Valor por defecto
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')  # Valor por defecto
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_DEFAULT_ACL = 'public-read'
 
